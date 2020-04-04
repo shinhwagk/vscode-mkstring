525ab4aad3ec7200b53c14f6085cecab56053043
shinhwagk <shanghai_sl@hotmail.com>
2020-04-04 17:39:37 +0800

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -12,9 +12,16 @@ export function activate(context: vscode.ExtensionContext) {
 			const sl = selection.start.line;
 			const el = selection.end.line;
 
-			const selectedLines = Array.from(Array(el - sl + 1).keys()).map(n => sl + n).map(l => document.lineAt(l).text)
+			let sl_l = "(", sl_r = ")", sl_s = ",", sl_c = "'", isSymbls = false;
+			const firstLineText = document.lineAt(sl).text;
+			if (firstLineText.startsWith('#')) {
+				const symbols = firstLineText.split(" ");
+				sl_l = symbols[1], sl_r = symbols[2], sl_s = symbols[3], sl_c = symbols[4], isSymbls = true;
+			}
 
-			const mkstring = "(" + selectedLines.map(t => "\'" + t).map(t => t + "\'").join(",") + ")";
+			const selectedLines = Array.from(Array(el - sl + 1).keys()).map(n => sl + n).map(l => document.lineAt(l).text);
+			if (isSymbls) { selectedLines.shift(); }
+			const mkstring = sl_l + selectedLines.map(t => sl_c + t).map(t => t + sl_c).join(sl_s) + sl_r;
 			editor.edit(editBuilder => editBuilder.replace(selection, mkstring));
 		}
 	});
